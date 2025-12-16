a778771e58cc725fb60ac54927512ca1a476d057
Chase Yturralde <Chase.Yturralde@affinipay.com>
2025-12-16 02:02:24 -0800

    Fixed issue with touchscreen UI thread.rlock issue

--- web_app.py
+++ web_app.py
@@ -8,6 +8,14 @@ Supports:
 - Arduino Uno R4 Minima (Renesas RA4M1 with native USB)
 - Arduino Uno R4 WiFi (Renesas RA4M1 with ESP32-S3)
 """
+# CRITICAL: Eventlet monkey-patching MUST happen before any other imports
+# This fixes thread.RLock errors when launched via subprocess (e.g., touchscreen UI)
+try:
+    import eventlet
+    eventlet.monkey_patch()
+except ImportError:
+    pass  # eventlet not installed, will use default threading
+
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 import serial
